91ba1e8378e3a1fd768290a1e38f937378c2d9f2
Caydn <Caydn.Lofton12@gmail.com>
2025-04-06 15:53:09 -0700

    Post Process Highlight

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -13,9 +13,9 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 	
 public:
+	AAuraEnemy();
+
+public:
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
 };
