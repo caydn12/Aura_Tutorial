4244efa79d2f4f8ed22ff8133e3a92803df1c81a
Caydn <Caydn.Lofton12@gmail.com>
2025-04-16 12:59:57 -0700

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Kickback Studio
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
