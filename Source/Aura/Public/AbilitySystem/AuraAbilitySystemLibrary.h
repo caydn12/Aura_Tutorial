3da6872bc7d78e247fbf1858824b708a1b356d3d
Caydn <Caydn.Lofton12@gmail.com>
2025-04-16 13:18:07 -0700

    Aura Ability System Blueprint Library

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,19 @@
+// Copyright Kickback Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+	static class UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
