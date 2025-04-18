7031f1b1e986674548e1fe0b21706ccba454f246
Caydn <Caydn.Lofton12@gmail.com>
2025-04-18 14:45:29 -0700

    Send Mouse Cursor Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+#include "AbilitySystemComponent.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -12,10 +13,44 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+
+	if (bIsLocallyControlled)
+	{
+		// On Client, Send mouse cursor data up to server
+		SendMouseCursorData();
+	}
+	else
+	{
+		// TODO: On the server, so listen for target data.
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	// Defines the prediction scope as the scope of this function. Everything done in this function should be predicted.
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
-	
+
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
 
-	ValidData.Broadcast(CursorHit.Location);
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		DataHandle,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey
+	);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -10,6 +10,8 @@ public class AuraEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
 }
