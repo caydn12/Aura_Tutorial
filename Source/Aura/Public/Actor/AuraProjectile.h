e647ee2c17ef5ce5971b15766ec9a0efeaae4c2c
Caydn <Caydn.Lofton12@gmail.com>
2025-04-17 21:04:53 -0700

    Aura Projectile

---
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -0,0 +1,34 @@
+// Copyright Kickback Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class AURA_API AAuraProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AAuraProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
