b46dca4e276d0c1f9eb4825ef17e44d3a68e41c1
Caydn <Caydn.Lofton12@gmail.com>
2025-07-02 15:58:13 -0700

    Cooldown Async Task

---
+++ Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,94 @@
+// Copyright Kickback Studio
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldownChange.h"
+#include "AbilitySystemComponent.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag)
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	if (IsValid(AbilitySystemComponent) && InCooldownTag.IsValid())
+	{
+		// To know when a cooldown has ended (Cooldown Tag Removed)
+		AbilitySystemComponent->RegisterGameplayTagEvent(
+			InCooldownTag,
+			EGameplayTagEventType::NewOrRemoved
+		).AddUObject(
+			WaitCooldownChange,
+			&UWaitCooldownChange::CooldownTagChanged
+		);
+
+		// To know when a cooldown has started (Cooldown Tag Added)
+		AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(
+			WaitCooldownChange,
+			&UWaitCooldownChange::OnActiveEffectAdded
+		);
+	}
+	else
+	{
+		WaitCooldownChange->EndTask();
+		WaitCooldownChange = nullptr;
+	}
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (IsValid(ASC))
+	{
+		ASC->RegisterGameplayTagEvent(
+			CooldownTag,
+			EGameplayTagEventType::NewOrRemoved
+		).RemoveAll(this);
+
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UWaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast(0.0f);
+	}
+	else
+	{
+
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+
+		const TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
+		float LongestTimeRemaining = 0.0f;
+		if (TimesRemaining.Num() > 0)
+		{
+			LongestTimeRemaining = TimesRemaining[0];
+			for (int32 i = 1; i < TimesRemaining.Num(); ++i)
+			{
+				if (TimesRemaining[i] > LongestTimeRemaining)
+				{
+					LongestTimeRemaining = TimesRemaining[i];
+				}
+			}
+		}
+		
+		CooldownStart.Broadcast(LongestTimeRemaining);
+	}
+}
