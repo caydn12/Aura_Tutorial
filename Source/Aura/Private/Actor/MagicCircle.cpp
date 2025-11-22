5d572d4c0b1710717452b22e492bd9f895ebb826
Caydn <Caydn.Lofton12@gmail.com>
2025-11-22 12:14:44 -0800

    Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,24 @@
+// Copyright Kickback Studio
+
+
+#include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>(TEXT("MagicCircleDecal"));
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,25 @@
+// Copyright Kickback Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMagicCircle();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
