1020bfb976b8cc27848594cbb28ab25f0971d7f1
Caydn <Caydn.Lofton12@gmail.com>
2025-12-10 15:25:13 -0800

    Constructing a View Model

--- Source/Aura/Public/UI/HUD/LoadMenuHUD.h
+++ Source/Aura/Public/UI/HUD/LoadMenuHUD.h
@@ -6,12 +6,26 @@
 #include "GameFramework/HUD.h"
 #include "LoadMenuHUD.generated.h"
 
-/**
- * 
- */
+class ULoadMenuWidget;
+class UMVVM_LoadMenu;
+
 UCLASS()
 class AURA_API ALoadMenuHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<ULoadMenuWidget> LoadMenuWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly, Category = "UI")
+	TObjectPtr<ULoadMenuWidget> LoadMenuWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UMVVM_LoadMenu> LoadMenuViewModelClass;
+
+	UPROPERTY(BlueprintReadOnly, Category = "UI")
+	TObjectPtr<UMVVM_LoadMenu> LoadMenuViewModel;
+protected:
+	virtual void BeginPlay() override;
 };
